abf921667a5668a463740b54356ed2e85ffe556e
SanjayaSamudra <sanjayasamudraelpitiya@gmail.com>
2024-07-07 12:11:07 +0530

    first commit

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ fetch("https://restcountries.com/v3.1/all")
 .then(data=>{
     data.forEach(element => {
         tabelBody += `<tr>
-                        <td>${element.name.common}
+                        <td><h1 class="head1" >${element.name.common}</h1>
                             <p> 
-                                Subregion : ${element.subregion} <br>
-                                Population : ${element.population} <br>
-                                <a class="btn btn-primary" href="${element.maps.googleMaps}">Go To Maps</a>
+                                <div>
+                                    Official Name : <h5>${element.name.official}</h5> <br>
+                                </div>
+                                <div>
+                                    Capital : ${element.capital} <br>
+                                </div>
+                                <div>
+                                    Population : ${element.population} <br>
+                                </div>
+                                <div>
+                                    <a class="btn btn-primary" href="${element.maps.googleMaps}">Go To Maps</a>
+                                </div>      
                             </p>
                         </td>
                         <td><img src="${element.flags.png}" alt=""></td>
@@ -25,3 +34,27 @@ fetch("https://restcountries.com/v3.1/all")
     tblCountries.innerHTML = tabelBody;
 
 })
+
+function searchCountry(){
+    let userInput = document.getElementById("txtInput").value;
+
+    let flagImg = document.getElementById("flagImg");
+    let name = document.getElementById("name");
+    let officialName = document.getElementById("officialName");
+    let region = document.getElementById("capital");
+    let population = document.getElementById("population");
+
+    fetch(`https://restcountries.com/v3.1/name/${userInput}`)
+    .then(res=>res.json())
+    .then(data=>{
+        data.forEach(obj=>{
+            console.log(obj);
+            flagImg.src=obj.flags.png
+            name.innerText=obj.name.common
+            officialName.innerText=obj.name.official
+
+            capital.innerText=obj.capital;
+            population.innerText=obj.population
+        })
+    })
+}
